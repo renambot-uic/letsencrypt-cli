e55a206973245134e774b0dfebe7340d3d4e2598
AJ ONeal <awesome@coolaj86.com>
2015-12-16 12:27:23 +0000

    standalone works

--- bin/letsencrypt.js
+++ bin/letsencrypt.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var cli = require('cli');
+var mkdirp = require('mkdirp');
 
 cli.parse({
   email: [ false, " Email used for registration and recovery contact. (default: null)", 'email' ]
@@ -9,8 +10,8 @@ cli.parse({
 , duplicate: [ false, " Allow getting a certificate that duplicates an existing one", 'boolean', false ]
 , 'agree-tos': [ false, " Agree to the Let's Encrypt Subscriber Agreement", 'boolean', false ]
 , debug: [ false, " show traces and logs", 'boolean', false ]
-, 'tls-sni-01-port': [ false, " Port number to perform tls-sni-01 challenge. Boulder in testing mode defaults to 5001. (default: 443 and 5001)", 'int' ]
-, 'http-01-port': [ false, " Port used in the SimpleHttp challenge.", 'int', 80 ]
+, 'tls-sni-01-port': [ false, " Port number to perform tls-sni-01 challenge. Boulder in testing mode defaults to 5001. (default: 443,5001)" ]
+, 'http-01-port': [ false, " Port used in the SimpleHttp challenge. (default: 80)" ]
 , 'rsa-key-size': [ false, " Size (in bits) of the RSA key.", 'int', 2048 ]
 , 'cert-path': [ false, " Path to where new cert.pem is saved", 'string',':conf/live/:hostname/cert.pem' ]
 , 'fullchain-path': [ false, " Path to where new fullchain.pem (cert + chain) is saved", 'string', ':conf/live/:hostname/fullchain.pem' ]
@@ -53,23 +54,53 @@ cli.main(function(_, options) {
     args[key] = val;
   });
 
-  var LE = require('letsencrypt');
-  var handlers;
-  
-  if (args.standalone) {
-    handlers = require('../lib/standalone');
-  }
-  else if (args.webrootPath) {
-    handlers = require('../lib/webroot');
+  if (args.domains) {
+    args.domains = args.domains.split(',');
   }
 
-  LE.create({}, handlers).register(args, function (err, results) {
+  if (args.tlsSni01Port) {
+    args.tlsSni01Port = args.tlsSni01Port.split(',').map(function (port) {
+      return parseInt(port, 10);
+    });
+  }
+
+  if (args.http01Port) {
+    args.http01Port = args.http01Port.split(',').map(function (port) {
+      return parseInt(port, 10);
+    });
+  }
+
+  mkdirp(args.configDir, function (err) {
     if (err) {
-      console.error(err.stack);
+      console.error("Could not create --config-dir '" + args.configDir + "':", err.code);
+      console.error("Try setting --config-dir '/tmp'");
       return;
     }
 
-    // should get back account, path to certs, pems, etc?
-    console.log(results);
+    var LE = require('letsencrypt');
+    var handlers;
+    
+    if (args.standalone) {
+      handlers = require('../lib/standalone');
+      handlers.startServers(args.http01Ports || [80], args.tlsSni01Port || [443, 5001]);
+    }
+    else if (args.webrootPath) {
+      handlers = require('../lib/webroot');
+    }
+
+    LE.create({}, handlers).register(args, function (err, results) {
+      if (err) {
+        console.error(err.stack);
+        return;
+      }
+
+      if (handlers.closeServers) {
+        handlers.closeServers();
+      }
+
+      // should get back account, path to certs, pems, etc?
+      console.log('results');
+      console.log(results);
+    });
   });
 });
